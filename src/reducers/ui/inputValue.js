9954731340694e1018c8b8ce063a9aeddcd5844c
Kendal Cormany <kendal.cormany@nexus.io>
2021-03-29 22:06:13 -0700

    Not sure why its not defined

--- src/reducers/ui/inputValue.js
+++ src/reducers/ui/inputValue.js
@@ -6,7 +6,7 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case TYPE.INITIALIZE: {
       const { moduleState } = action.payload;
-      return (moduleState && moduleState.ui.inputValue) || initialState;
+      return (moduleState && moduleState.ui?.inputValue) || initialState;
     }
 
     case TYPE.UPDATE_INPUT:
