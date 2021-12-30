d47d972f9047b54ff4840050a5bb32476dba0134
Krysto <quy.p.hoang@gmail.com>
2021-12-30 16:41:37 +0700

    cleanup

--- src/reducers/settings/exchangeRate.js
+++ src/reducers/settings/exchangeRate.js
@@ -5,7 +5,6 @@ const initialState = false;
 export default (state = initialState, action) => {
   switch (action.type) {
     case TYPE.UPDATE_EXCHANGE_RATE: {
-      console.error(action);
       return action.payload;
     }
     default:
