cc16a86eddb04489b1589bfa37c7ee7cb9b98068
Krysto <quy.p.hoang@gmail.com>
2022-01-18 10:11:48 +0700

    replaced actions with nexus-module actions

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,25 +1,5 @@
 import * as TYPE from './types';
 
-export const initialize = (data) => ({
-  type: TYPE.INITIALIZE,
-  payload: data,
-});
-
-export const updateCoreInfo = (coreInfo) => ({
-  type: TYPE.UPDATE_CORE_INFO,
-  payload: coreInfo,
-});
-
-export const updateTheme = (theme) => ({
-  type: TYPE.UPDATE_THEME,
-  payload: theme,
-});
-
-export const updateUserStatus = (userStatus) => ({
-  type: TYPE.UPDATE_USER_STATUS,
-  payload: userStatus,
-});
-
 export const showConnections = () => ({
   type: TYPE.SHOW_CONNECTIONS,
 });
--- src/index.js
+++ src/index.js
@@ -1,42 +1,11 @@
 import { Provider } from 'react-redux';
+import { listenToWalletData } from 'nexus-module';
 
 import configureStore from './configureStore';
 import App from './App';
-import {
-  initialize,
-  updateCoreInfo,
-  updateTheme,
-  updateUserStatus,
-} from './actions/actionCreators';
 
 const store = configureStore();
-
-const {
-  libraries: { React, ReactDOM },
-  utilities: {
-    onceInitialize,
-    onCoreInfoUpdated,
-    onThemeUpdated,
-    onUserStatusUpdated,
-  },
-} = NEXUS;
-
-onceInitialize((data) => {
-  store.dispatch(initialize(data));
-});
-
-onCoreInfoUpdated((coreInfo) => {
-  store.dispatch(updateCoreInfo(coreInfo));
-});
-
-onThemeUpdated((theme) => {
-  store.dispatch(updateTheme(theme));
-});
-
-onUserStatusUpdated((userStatus) => {
-  //if null == not logged in
-  store.dispatch(updateUserStatus(userStatus));
-});
+listenToWalletData(store);
 
 ReactDOM.render(
   <Provider store={store}>
