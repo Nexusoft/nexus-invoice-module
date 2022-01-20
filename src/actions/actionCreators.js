ad74c860ef9b21559f99d5478a769b5cf257bbdc
Krysto <quy.p.hoang@gmail.com>
2022-01-20 17:32:15 +0700

    removed redundant actions folder

--- src/actions/actionCreators.js
+++
@@ -1,9 +0,0 @@
-import * as TYPE from './types';
-
-export const showConnections = () => ({
-  type: TYPE.SHOW_CONNECTIONS,
-});
-
-export const hideConnections = () => ({
-  type: TYPE.HIDE_CONNECTIONS,
-});
