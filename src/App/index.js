22d3eb8b54ccde9f3a222adc2b193f580f16e8b0
Krysto <quy.p.hoang@gmail.com>
2023-08-08 21:21:29 +0700

    added table styles for the next wallet update

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,8 @@
 import { useSelector } from 'react-redux';
 import { ModuleWrapper } from 'nexus-module';
+import { Global } from '@emotion/react';
 
+import tableStyles from 'tableStyles';
 import Main from './Main';
 import Modals from './Modals';
 
@@ -10,6 +12,7 @@ export default function App() {
 
   return (
     <ModuleWrapper initialized={initialized} theme={theme}>
+      <Global styles={tableStyles} />
       <Main />
       <Modals />
     </ModuleWrapper>
